ccaa732080da71438d1971cda1bee5aab2bc6596
Weifeng Hu <weifenghu@Weifengs-MacBook-Pro.local>
2019-02-18 22:49:41 -0800

    trim input

--- src/SSGroundTruth.java
+++ src/SSGroundTruth.java
@@ -6,13 +6,14 @@ import java.io.FileNotFoundException;
 public class SSGroundTruth {
     // read packet input from source file
     private static ArrayList<Packet> read_csv_file(String filepath){
-        ArrayList<Packet> inputPackets = new ArrayList<>();
+        ArrayList<Packet> inputPackets = new ArrayList<Packet>();
         try {
             Scanner scanner = new Scanner(new File(filepath));
             while (scanner.hasNext()){
                 String packet_info = scanner.nextLine();
-                String[] field = packet_info.split(" ");
-                inputPackets.add(new Packet(field[0], field[1], field[2], field[3], field[4]));
+                packet_info = packet_info.trim();
+                String[] field = packet_info.split("\\s+");
+                inputPackets.add(new Packet(field[0], field[2], field[3], field[4], field[5]));
             }
         }
         catch (FileNotFoundException e){
@@ -23,18 +24,18 @@ public class SSGroundTruth {
 
     // construct spreader hashtable from input array
     private static HashMap<String, Integer> getSpreaders(ArrayList<Packet>inputPackets){
-        HashMap<String, Integer> spreaders = new HashMap<>();
+        HashMap<String, Integer> spreaders = new HashMap<String, Integer>();
         return spreaders;
     }
 
     // get top K superspreader
     private static ArrayList<String> topKSuperspreader(HashMap<String, Integer> spreaders, int K){
-        ArrayList<String> topk = new ArrayList<>();
+        ArrayList<String> topk = new ArrayList<String>();
         return topk;
     }
 
     public static void main(String[] args){
-        //ArrayList<Packet> input = read_csv_file("/Users/weifenghu/Desktop/MSCS/W19/CSE222A/superspreader/src/test.csv");
+        ArrayList<Packet> input = read_csv_file("/Users/weifenghu/Desktop/MSCS/W19/CSE222A/superspreader/src/test1.csv");
         System.out.print("Done");
     }
 }
